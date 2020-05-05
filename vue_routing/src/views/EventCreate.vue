b6c0112e240eaa961870e35f4f3a2967c4f5e684
Michelle <michelledewit1.mdw@gmail.com>
2020-05-05 16:56:13 +0200

    Added vue_routing project

---
+++ vue_routing/src/views/EventCreate.vue
@@ -0,0 +1,6 @@
+<template>
+<div class="app">
+  <h1>About</h1>
+  <h2>Menu Routing is working</h2>
+</div>
+</template>
---
+++ vue_routing/src/views/EventList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Events List</h1>
+    <EventCard />
+    <BaseIcon name="activity" />
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
